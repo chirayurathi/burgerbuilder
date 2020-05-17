4f9452e87ab10a68e43f1604d657efcb438d31d3
chirayurathi <chirayurathi4@gmail.com>
2020-05-17 07:51:26 +0530

    Updates

---
+++ precache-manifest.7a30165306c57a216f730306d7415f79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ef844453643f4b9dbc1bd605e0b8aa",
+    "url": "/burgerbuilder/index.html"
+  },
+  {
+    "revision": "12ecc3bc5e7833ce8216",
+    "url": "/burgerbuilder/static/css/main.ba4edea1.chunk.css"
+  },
+  {
+    "revision": "24f29ae7e095589244fb",
+    "url": "/burgerbuilder/static/js/2.bd394bc7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burgerbuilder/static/js/2.bd394bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ecc3bc5e7833ce8216",
+    "url": "/burgerbuilder/static/js/main.38bdfafb.chunk.js"
+  },
+  {
+    "revision": "3693e49165dbc7756199",
+    "url": "/burgerbuilder/static/js/runtime-main.595a6bde.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerbuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
